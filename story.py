0314e20908036e3b12d809f5bbb34e026013a465
scotchfield <swrittenb@gmail.com>
2016-11-28 18:09:58 -0500

    Adds movement and some story output

--- story.py
+++ story.py
@@ -1,5 +1,8 @@
 import json
+import random
 import sys
+
+VERBOSE = False
 
 
 def dead( st, line=False ):
@@ -12,6 +15,30 @@ def dead( st, line=False ):
 
     sys.exit(1)
 
+def getKey( key, value, obj ):
+    for x in obj:
+        if key in x and x[key] == value:
+            return x
+
+    return None
+
+def updateStory( world, agent, location ):
+    output = []
+
+    output.append( 'I\'m standing in ' + location['name'] + '.' )
+    if 'objects' in location:
+        thing = random.choice( location['objects'] )
+        output.append( 'As I look around, I see ' + thing['name'] + ' in the room with me.' )
+
+    if 'movement' in location:
+        move = random.choice( location['movement'].keys() )
+        new_location = getKey( 'id', location['movement'][move], world['locations'] )
+        output.append( 'I decide to leave, and go ' + move + ', taking me to ' + location['movement'][move] + '.' )
+
+    print '. '.join( output )
+
+    return new_location
+
 
 try:
     input_filename = sys.argv[1]
@@ -20,4 +47,13 @@ except IndexError:
 input_file = open( input_filename, 'r' )
 world = json.load( input_file )
 
-print( world )
+if VERBOSE:
+    print( world )
+
+agent = random.choice( world['agents'] )
+location = random.choice( world['locations'] )
+
+print 'My name is ' + agent['name'] + '. This is my story.'
+
+for i in range( 2 ):
+    location = updateStory( world, agent, location )
